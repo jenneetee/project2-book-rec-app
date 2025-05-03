8dad64a82fdb2af9aaaf45186a379df2cf23bf00
mariamomer135 <140013469+mariamomer135@users.noreply.github.com>
2025-05-03 15:08:05 -0400

    Update login_screen.dart

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'signup_screen.dart';
-import 'profile_screen.dart'; // after login go to profile
-
+import 'profile_screen.dart'; 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
